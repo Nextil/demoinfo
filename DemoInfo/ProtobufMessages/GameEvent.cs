2fe96e139be59076d0de11db34e4fd8768777881
main() <main@ehvag.de>
2015-02-20 01:27:49 +0100

    Quick and dirty implementation of GEH keys

--- DemoInfo/ProtobufMessages/GameEvent.cs
+++ DemoInfo/ProtobufMessages/GameEvent.cs
@@ -8,15 +8,25 @@ namespace EHVAG.DemoInfo.ProtobufMessages
 {
     public struct GameEvent
     {
+        public struct QuickAndDirtyKey
+        {
+            public Int32 Type;
+            public String String;
+            public float Float;
+            public Int32 Long;
+            public Int32 Short;
+            public Int32 Byte;
+            public bool Bool;
+            public UInt64 UInt64;
+        }
+
         public string EventName;
         public Int32 EventId;
-        public IList<object> Keys;
+        public IList<QuickAndDirtyKey> Keys;
 
         internal void Parse(IBitStream bitstream, DemoParser parser)
         {
-            throw new NotImplementedException();
-
-            Keys = new List<object>();
+            Keys = new List<QuickAndDirtyKey>();
             while (!bitstream.ChunkFinished)
             {
                 var desc = bitstream.ReadProtobufVarInt();
@@ -32,57 +42,60 @@ namespace EHVAG.DemoInfo.ProtobufMessages
                 }
                 else if ((wireType == 2) && (fieldnum == 3))
                 {
+                    var qad = new QuickAndDirtyKey();
                     bitstream.BeginChunk(bitstream.ReadProtobufVarInt() * 8);
-                    /*
-                     * Hope and pray that gaben is once again nice to us and
-                     * sends 'type' first, then the respective member, then NOTHING.
-                     */
-                    desc = bitstream.ReadProtobufVarInt();
-                    wireType = desc & 7;
-                    fieldnum = desc >> 3;
-                    if ((wireType != 0) || (fieldnum != 1))
-                        throw new InvalidDataException("Lord Gaben wasn't nice to us :/");
-
-                    var typeMember = bitstream.ReadProtobufVarInt();
-                    desc = bitstream.ReadProtobufVarInt();
-                    wireType = desc & 7;
-                    fieldnum = desc >> 3;
-
-                    if (fieldnum != (typeMember + 1))
-                        throw new InvalidDataException("Lord Gaben wasn't nice to us :/ (srsly wtf!?)");
-
-                    switch (typeMember)
-                    {
-                        case 1: // string
-                            if (wireType != 2)
-                                throw new InvalidDataException("proto definition differs");
-                            Keys.Add(bitstream.ReadProtobufString());
-                            break;
-                        case 2: // float
-                            if (wireType != 5)
-                                throw new InvalidDataException("proto definition differs");
-                            Keys.Add(bitstream.ReadFloat());
-                            break;
-                        case 3: // long
-                        case 4: // short
-                        case 5: // byte
-                            if (wireType != 0)
-                                throw new InvalidDataException("proto definition differs");
-                            Keys.Add(bitstream.ReadProtobufVarInt());
-                            break;
-                        case 6: // bool
-                            if (wireType != 0)
-                                throw new InvalidDataException("proto definition differs");
-                            Keys.Add(bitstream.ReadProtobufVarInt() != 0);
-                            break;
-                        default:
-                            throw new InvalidDataException("Looks like they introduced a new type");
+                    while (!bitstream.ChunkFinished) {
+                        desc = bitstream.ReadProtobufVarInt();
+                        wireType = desc & 7;
+                        fieldnum = desc >> 3;
+                        switch (fieldnum)
+                        {
+                            case 1:
+                                if (wireType != 0)
+                                    throw new InvalidDataException();
+                                qad.Type = bitstream.ReadProtobufVarInt();
+                                break;
+                            case 2:
+                                if (wireType != 2)
+                                    throw new InvalidDataException();
+                                qad.String = bitstream.ReadProtobufString();
+                                break;
+                            case 3:
+                                if (wireType != 5)
+                                    throw new InvalidDataException();
+                                qad.Float = bitstream.ReadFloat();
+                                break;
+                            case 4:
+                                if (wireType != 0)
+                                    throw new InvalidDataException();
+                                qad.Long = bitstream.ReadProtobufVarInt();
+                                break;
+                            case 5:
+                                if (wireType != 0)
+                                    throw new InvalidDataException();
+                                qad.Short = bitstream.ReadProtobufVarInt();
+                                break;
+                            case 6:
+                                if (wireType != 0)
+                                    throw new InvalidDataException();
+                                qad.Byte = bitstream.ReadProtobufVarInt();
+                                break;
+                            case 7:
+                                if (wireType != 0)
+                                    throw new InvalidDataException();
+                                qad.Bool = bitstream.ReadProtobufVarInt() != 0;
+                                break;
+                            case 8:
+                                if (wireType != 0)
+                                    throw new InvalidDataException();
+                                qad.UInt64 = (ulong)bitstream.ReadProtobufVarInt();
+                                break;
+                            default:
+                                throw new NotImplementedException();
+                        }
                     }
-
-                    if (!bitstream.ChunkFinished)
-                        throw new InvalidDataException("Lord Gaben tricked us! D:");
-
                     bitstream.EndChunk();
+                    Keys.Add(qad);
                 }
                 else
                     throw new InvalidDataException();
