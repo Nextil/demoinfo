cf59f82d94ed4be5ad72645323e27271eb284606
moritzuehling <github@moritzuehling.de>
2015-03-13 12:48:55 +0100

    Added bot_takeover, fixed build.

---
+++ DemoInfo/DemoPackets/GameEvents/Events/BotTakeover.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using EHVAG.DemoInfo.Utils.Reflection;
+using EHVAG.DemoInfo.Edicts;
+
+namespace EHVAG.DemoInfo.DemoPackets.GameEvents.Events
+{
+    [ValveEvent("bot_takeover")]
+    public class BotTakeover : BaseEvent
+    {
+        [NetworkedProperty("userid")]
+        NetworkedVar<int> UserID { get; set; }
+
+        [NetworkedProperty("botid")]
+        NetworkedVar<int> BotID { get; set; }
+
+
+
+        [NetworkedProperty("index")]
+        NetworkedVar<int> Index { get; set; }
+
+        /// <summary>
+        /// The Player 
+        /// </summary>
+        /// <value>The player.</value>
+        public CSPlayer Player 
+        { 
+            get 
+            {
+                return EventInfo.Parser.GameState.GetPlayerByUserID(UserID);
+            } 
+        }
+
+        /// <summary>
+        /// The Bot 
+        /// </summary>
+        /// <value>The player.</value>
+        public CSPlayer Bot 
+        { 
+            get 
+            {
+                return EventInfo.Parser.GameState.GetPlayerByUserID(BotID);
+            } 
+        }
+
+        internal override void HandleYourself()
+        {
+            EventInfo.Parser.Events.RaiseBotTakeover(this);
+        }
+    }
+}
+
