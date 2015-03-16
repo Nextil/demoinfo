2643565da4d528b69a09d604f4f8d96f2966f9b6
moritzuehling <github@moritzuehling.de>
2015-03-16 12:16:17 +0100

    Added basic CWeaponCSBaseGun to give all weapons an instance.

---
+++ DemoInfo/Edicts/CSWeapon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using EHVAG.DemoInfo.Utils.Reflection;
+
+namespace EHVAG.DemoInfo.Edicts
+{
+    [ServerClass("CWeaponCSBaseGun")]
+    public class CSWeapon : BaseEntity
+    {
+        [NetworkedProperty("m_hOwner")]
+        NetworkedVar<int> ownerRef { get; set; }
+
+        public EntityInformation Owner
+        {
+            get {
+                return EntityInfo.Parser.RawData.Entities[ownerRef & (2048 - 1)];
+            }
+        }
+
+        internal override void FullyInitialized()
+        {
+            base.FullyInitialized();
+        }
+    }
+}
+
